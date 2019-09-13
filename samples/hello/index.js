e2778d9ce6b6a5a63e3f46c326b4110c597e0022
Jan Illetschko <jan.illetschko@3kraft.com>
2019-09-13 11:33:29 +0200

    fixes hello sample

--- samples/hello/index.js
+++ samples/hello/index.js
@@ -2,10 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/hello/:username', (req, res) => {
-    res.setH
-    //res.status(200).type('text').send(`Hello ${req.params['username']}`);
-    res.status(200).json(`Hello ${req.params['username']}`);
+app.get("/hello/:username", (req, res) => {
+    res.status(200).json(`Hello ${req.params["username"]}`);
 })
 
 const port = 3000;
